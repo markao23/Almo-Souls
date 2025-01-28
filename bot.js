a713a1ce46bedb4c4b04cfd0b848e0837f0c6430
markao23 <77198729+markao23@users.noreply.github.com>
2025-01-28 17:14:08 -0300

    eu add ah asincronagem dos comandos na pasta commands

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, GatewayIntentBits, Collection } = require('discord.js')
 require('dotenv').config()
 
 const client = new Client({
@@ -8,9 +8,39 @@ const client = new Client({
         GatewayIntentBits.MessageContent
     ]
 })
+client.commands = new Collection()
+
+// const pra carrecar os comandos 
+const path = require('path')
+const fs = require('fs')
+
+const commandFiles = fs.readFileSync(path.join(__dirname, "commands")).filter(file => file.endsWith('.js'))
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`)
+    client.command.set(command.name, command)
+}
 
 client.on('ready', () => {
     console.log(`Bot logado ${client.user.tag}`);
+})
+
+client.on('messageCreate', async (message) => {
+    if(message.author.bot) return;
+    if(!message.content.startsWith('b@')) return
+
+    const args = message.content.slice(1).trim().split(/ +/)
+    const commandName = args.shift().toLocaleLowerCase()
+
+    const command = client.commands.get(commandName);
+
+    if (command) {
+        try {
+            await command.execute(message)
+        } catch (error) {
+            console.error(error);
+            await message.reply('Houve um erro ao executar esse comando!')
+        }
+    }
 })
 
 client.login(process.env.TOKEN);
