adc582929e0343f1e0c7fb741e1506d6aaf4a22f
markao23 <77198729+markao23@users.noreply.github.com>
2025-01-28 17:42:08 -0300

    eu coloquei o evento pra os comandos o pra registrar tbm

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js')
+const { Client, GatewayIntentBits, Collection, REST, Routes } = require('discord.js')
 require('dotenv').config()
 
 const client = new Client({
@@ -9,6 +9,7 @@ const client = new Client({
     ]
 })
 client.commands = new Collection()
+client.slashCommands = new Collection();
 
 // const pra carrecar os comandos 
 const path = require('path')
@@ -20,13 +21,50 @@ for(const file of commandFiles){
     client.command.set(command.name, command)
 }
 
+//carregar os comandos de barra
+const slashCommandFiles = fs.readFileSync(path.join(__dirname, 'slashCommands')).filter(file => file.endsWith('.js'))
+for(const file of slashCommandFiles){
+    const slashCommand = require(`./slashCommands/${file}`)
+    client.slashCommand.set(slashCommand.name, slashCommand)
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
+
+(async () => {
+    try {
+        console.log('Iniciando o registro de comandos de barra...')
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
+            body: slashCommandFiles.map(file => require(`./slashCommands/${file}`)),
+        });
+        console.log('Comandos de barra registrados com sucesso!');
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+// evento pra os comandos de barra 
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isCommand()) {
+        const command = client.slashCommands.get(interaction.commandName)
+        if (command) {
+            try {
+                await command.execute(interaction)
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({ content: 'Houve um erro ao executar esse comando!', ephemeral: true });
+            }
+        }
+    }
+})
+
 client.on('ready', () => {
     console.log(`Bot logado ${client.user.tag}`);
 })
 
 client.on('messageCreate', async (message) => {
     if(message.author.bot) return;
-    if(!message.content.startsWith('b@')) return
+    const prefix = 'b@';
+    if(!message.content.startsWith(prefix)) return
 
     const args = message.content.slice(1).trim().split(/ +/)
     const commandName = args.shift().toLocaleLowerCase()
